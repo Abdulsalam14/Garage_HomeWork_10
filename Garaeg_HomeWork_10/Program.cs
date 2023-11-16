150f59df021bcc572fca006e18a9eed6e73c4c5e
Abdulsalam14 <abdullayevabdulsalam@gmail.com>
2023-11-16 16:11:24 +0400

    Add project files.

---
+++ Garaeg_HomeWork_10/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Student tural = new Student();
+Console.WriteLine(tural.Age); 
+
+Student suleyman = new Student();
+Console.WriteLine(suleyman.Age); 
+
+Student gafar = new Student();
+Console.WriteLine(gafar.Age);
+
+Student semed = new Student();
+Console.WriteLine(semed.Age);
+
+public class Student
+{
+    private static int _age = 0;
+    public int Age { get; set; }
+    public Student()
+    {
+        Age = ++_age;
+    }
+}
+
